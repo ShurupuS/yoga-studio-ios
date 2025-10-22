3aea1470d3772d6d208b2304dcc541d7b5feb47c
Aleksei Kochetov <shurupus@gmail.com>
2025-10-22 20:58:46 +0200

    Initial Commit

---
+++ yoga-studio-app/yoga_studio_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  yoga_studio_appApp.swift
+//  yoga-studio-app
+//
+//  Created by Alexey Kochetov on 22.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct yoga_studio_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
